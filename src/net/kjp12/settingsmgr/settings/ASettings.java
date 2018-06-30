053157b27c14bfd4c6a8403df024d8607a46fd4d
KJP12 <kjp12.k.joy.programer12@gmail.com>
2018-06-30 05:51:52 -0700

    oops

--- src/net/kjp12/settingsmgr/settings/ASettings.java
+++ src/net/kjp12/settingsmgr/settings/ASettings.java
@@ -1,9 +1,6 @@
 package net.kjp12.settingsmgr.settings;//Created on 6/30/18.
 
-import com.google.gson.annotations.Expose;
-
 public class ASettings implements Dirty {
-    @Expose(deserialize = false, serialize = false)
     private transient boolean dirty;
 
     @Override
